80c5c59b12c3d5f45855403de6896839c121f155
darektoa <darektoa@gmail.com>
2022-10-14 14:40:21 +0700

    Feat: Using browser router from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import './styles/index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 )
