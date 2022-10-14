123ead48434037f6d22ff976e51596c75f8a5d7a
darektoa <darektoa@gmail.com>
2022-10-14 21:15:22 +0700

    Chore: Add dummy questions data

---
+++ src/data/QuestionData.js
@@ -0,0 +1,73 @@
+const QuestionData = [
+    {
+        id: 1,
+        question: 'Siapakah presiden-presiden Indonesia?',
+        type: 'multiple',
+        options: [
+            {
+                id: 1,
+                label: 'B. J. Habibie'
+            },
+            {
+                id: 2,
+                label: 'Ir. Soekarno'
+            },
+            {
+                id: 3,
+                label: 'K. H. Abdurrahman Wahid'
+            },
+            {
+                id: 4,
+                label: 'Drs. Mohmmad Hatta'
+            }
+        ]
+    },
+    {
+        id: 2,
+        question: '1 ÷ 0 = ...',
+        type: 'single',
+        options: [
+            {
+                id: 5,
+                label: 'Tak Terhingga'
+            },
+            {
+                id: 6,
+                label: '0'
+            },
+            {
+                id: 7,
+                label: 'Tak Terdefinisikan'
+            },
+            {
+                id: 8,
+                label: '1'
+            }
+        ]
+    },
+    {
+        id: 3,
+        question: 'Pilih mata pelajaran yang kamu sukai',
+        type: 'multiple',
+        options: [
+            {
+                id: 9,
+                label: 'Matematika'
+            },
+            {
+                id: 10,
+                label: 'Bahasa Inggris'
+            },
+            {
+                id: 11,
+                label: 'Ilmu Pengetahuan Alam'
+            },
+            {
+                id: 12,
+                label: 'Ilmu Pengetahuan Sosial'
+            }
+        ]
+    },
+];
+
+export default QuestionData;
